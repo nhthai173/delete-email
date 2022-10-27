6ef928cd52e747d501887962a29ebcbc9d6e5930
Thái <62926491+nhthai173@users.noreply.github.com>
2022-10-27 17:55:28 +0700

    Rename

--- info.js
+++ info.js
@@ -2,7 +2,7 @@ function getLibraryInfo () {
 
   return { 
     info: {
-      name:'DeleteEmailFilter',
+      name:'DeleteEmailFiltered',
       version:'1.0.0',
       id: '1diMNfkgtlWQDwmLjO5YEdGOEDt9pQfIqnc1MZgl1I_jC20fp96K5du1g',
       description:'Delete email tools',
