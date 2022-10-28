38392a4907669347c633d305ea11e32ab1ad01a7
Thái <62926491+nhthai173@users.noreply.github.com>
2022-10-28 20:57:38 +0700

    Improved code
    - Now we can get all messages from anywhere.
    - Improved code.

---
+++ functions.js
@@ -0,0 +1,209 @@
+// Config for global sheet
+const GLOBAL_FILTER_SHEET = {
+  id: '1r6VXfO9Xc1xjmrOpBzd-4EAAaMKKx3E9cIxmUfGq7K0',
+  name: 'Global',
+  path: {
+    from: 0,
+    subject: 1,
+    body: 2,
+    time: 3,
+    id: 4
+  }
+}
+
+
+
+/**
+ * Match the time
+ * @param{string} timeStr time string
+ * @param{number} mailTime unix timestamp
+ */
+function matchTime(timeStr = '', mailTime = 0){
+  const base = 24*3600000
+  const m = {
+    'day': base,
+    'week': 7*base,
+    'month': 30*base,
+    'quarter': 90*base,
+    'year': 365*base,
+    'd': base,
+    'w': 7*base,
+    'm': 30*base,
+    'y': 365*base
+  }
+  if(timeStr){
+    for(const i in m){
+      if(timeStr.indexOf(i)){
+        let time = parseFloat(timeStr.substring(0, timeStr.indexOf(i)))
+        if(time && !TTools.isEmptyVariable(mailTime)){
+          time = time*m[i]
+          const curr = new Date().getTime()
+          if(curr >= mailTime+time){
+            return true
+          }
+        }
+        return false
+      }
+    }
+  }
+  return true
+}
+
+
+
+function matchFilter(mailDetail = {}, filter = {}, customFilter = {}, fields = []){
+  let valid = []
+  if(!TTools.isValidArray(fields)) fields = ['from', 'subject', 'body']
+  if(!TTools.isValidObject(mailDetail)) return false
+
+  // Match text
+  for(const j in fields){
+    const field = fields[j]
+    if(filter && !TTools.isEmptyVariable(filter[field])){
+      if (customFilter && customFilter[ field ] && typeof customFilter[ field ] === 'function'){
+        if (customFilter[ field ](mailDetail)){
+          valid.push(true)
+        }else{
+          valid.push(false)
+        }
+      }else{
+        let matched = 0
+        const keys = filter[field].split(',')
+        keys.forEach(s => {
+          const raw = TTools.toRawText(mailDetail[ field ])
+          if (raw.includes(TTools.toRawText(s))) matched++
+        })
+        if(matched > 0 && matched == keys.length){
+          valid.push(true)
+        }else{
+          valid.push(false)
+        }
+      }
+    }
+  }
+
+  // Match time
+  if (filter && !TTools.isEmptyVariable(filter.time)) {
+    if(mailDetail.date && mailDetail.date.getTime && typeof mailDetail.date.getTime === 'function'){
+      if (matchTime(filter.time, mailDetail.date.getTime())) {
+        valid.push(true)
+      } else {
+        valid.push(false)
+      }
+    }
+  }
+
+  return valid.length && valid.reduce((prv, cur) => prv && cur, true)
+}
+
+
+function globalFilterMatch(mailDetail = {}, filter = []){
+  let output = {
+    matched: false,
+    id: null
+  }
+
+  if(!TTools.isValidObject(mailDetail)) return output
+  if(!TTools.isValidArray(filter)){
+    filter =  getGlobalFilter()
+  }
+  if(!TTools.isValidArray(filter)) return output
+
+  for(const i in filter){
+    if(matchFilter(mailDetail, filter[i])){
+      output.matched = true
+      output.id = filter[i]['id'] || null
+      break
+    }
+  }
+
+  return output
+}
+
+
+
+function getFilterFromSheet(sheetId = '', sheetName = '', path = {}){
+  let output = []
+
+  if(sheetId) return output
+  if(sheetName) return output
+  if(!TTools.isValidObject(path)) return output
+
+  output = TTools.BillSheet({
+    sheetId,
+    sheetName,
+    path
+  }).getAsJSON()
+
+  return output
+}
+
+
+
+function getGlobalFilter(){
+  if(!TTools.isValidObject(GLOBAL_FILTER_SHEET)) return []
+  return getFilterFromSheet(
+    GLOBAL_FILTER_SHEET.id,
+    GLOBAL_FILTER_SHEET.name,
+    GLOBAL_FILTER_SHEET.path)
+}
+
+
+function getAllMessages({
+  includeSpam = true,
+  includeTrash = false,
+  limit = 0
+} = {},
+callback){
+
+  let msgs = []
+  const opt = {}
+  
+  if(includeSpam || includeTrash) opt.includeSpamTrash = true
+  if(limit > 0) opt.maxResults = limit
+
+  // Try with Gmail API
+  try {
+    let pageToken
+    do {
+      const messageList = Gmail.Users.Messages.list('me', {
+        pageToken: pageToken,
+        ...opt
+      })
+      if (messageList.messages && messageList.messages.length) {
+        messageList.messages.forEach(msg => {
+          const id = msg.id
+          try{
+            const message = GmailApp.getMessageById(id)
+            if(TTools.isValidObject(message)){
+              if(callback && typeof callback === 'function'){
+                if(!includeTrash && message.isInTrash()) return
+                callback(message)
+              }
+              msgs.push(message)
+            }
+          }catch(err){ console.warn({type: 'error', msgId: id, detail: err}) }
+        })
+      }
+      pageToken = messageList.nextPageToken
+    } while (pageToken)
+  } catch (err) { console.warn(err) }
+
+  // Use basic GmailApp
+  if(!TTools.isValidArray(msgs)){
+    try{
+      GmailApp.getInboxThreads().forEach(thread => {
+        const messages = thread.getMessages()
+        if(callback && typeof callback === 'function'){
+          messages.forEach(msg => {
+            if(!includeTrash && msg.isInTrash()) return
+            callback(msg)
+          })
+        }
+        msgs = [...msgs, ...messages]
+      })
+    } catch(err) { console.warn(err) }
+  }
+
+  return msgs
+}
--- main.js
+++ main.js
@@ -1,133 +1,9 @@
-// Config for global sheet
-const GLOBAL_FILTER_SHEET = {
-  id: '1r6VXfO9Xc1xjmrOpBzd-4EAAaMKKx3E9cIxmUfGq7K0',
-  name: 'Global',
-  path: {
-    from: 0,
-    subject: 1,
-    body: 2,
-    time: 3,
-    id: 4
-  }
-}
-
-
-/**
- * Match the time
- * @param{string} timeStr time string
- * @param{number} mailTime unix timestamp
- */
-function matchTime(timeStr = '', mailTime = 0){
-  const base = 24*3600000
-  const m = {
-    'day': base,
-    'week': 7*base,
-    'month': 30*base,
-    'quarter': 90*base,
-    'year': 365*base,
-    'd': base,
-    'w': 7*base,
-    'm': 30*base,
-    'y': 365*base
-  }
-  if(timeStr){
-    for(const i in m){
-      if(timeStr.indexOf(i)){
-        let time = parseFloat(timeStr.substring(0, timeStr.indexOf(i)))
-        if(time && !TTools.isEmptyVariable(mailTime)){
-          time = time*m[i]
-          const curr = new Date().getTime()
-          if(curr >= mailTime+time){
-            return true
-          }
-        }
-        return false
-      }
-    }
-  }
-  return true
-}
-
-
-
-function matchFilter(mailDetail = {}, filter = {}, customFilter = {}, fields = []){
-  let valid = []
-  if(!TTools.isValidArray(fields)) fields = ['from', 'subject', 'body']
-  if(!TTools.isValidObject(mailDetail)) return false
-
-  // Match text
-  for(const j in fields){
-    const field = fields[j]
-    if(filter && !TTools.isEmptyVariable(filter[field])){
-      if (customFilter && customFilter[ field ] && typeof customFilter[ field ] === 'function'){
-        if (customFilter[ field ](mailDetail)){
-          valid.push(true)
-        }else{
-          valid.push(false)
-        }
-      }else{
-        let matched = 0
-        const keys = filter[field].split(',')
-        keys.forEach(s => {
-          const raw = TTools.toRawText(mailDetail[ field ])
-          if (raw.includes(TTools.toRawText(s))) matched++
-        })
-        if(matched > 0 && matched == keys.length){
-          valid.push(true)
-        }else{
-          valid.push(false)
-        }
-      }
-    }
-  }
-
-  // Match time
-  if (filter && !TTools.isEmptyVariable(filter.time)) {
-    if(mailDetail.date && mailDetail.date.getTime && typeof mailDetail.date.getTime === 'function'){
-      if (matchTime(filter.time, mailDetail.date.getTime())) {
-        valid.push(true)
-      } else {
-        valid.push(false)
-      }
-    }
-  }
-
-  return valid.length && valid.reduce((prv, cur) => prv && cur, true)
-}
-
-
-function globalFilter(mailDetail = {}){
-  let output = {
-    matched: false,
-    id: null
-  }
-
-  if(!TTools.isValidObject(mailDetail)) return output
-  
-  const data = TTools.BillSheet({
-    sheetId: GLOBAL_FILTER_SHEET.id,
-    sheetName: GLOBAL_FILTER_SHEET.name,
-    path: GLOBAL_FILTER_SHEET.path
-  }).getAsJSON()
-  if(!TTools.isValidArray(data)) return output
-
-  for(const i in data){
-    if(matchFilter(mailDetail, data[i])){
-      output.matched = true
-      output.id = data[i]['id'] || null
-      break
-    }
-  }
-
-  return output
-}
-
-
-
 function EmailDetailFilter(message, {
+  debug = false,
   filter = [],
   customFilter = {},
   useGlobalFilter = true,
+  globalFilter = [],
   customFuntions = {}
 } = {}){
 
@@ -144,6 +20,16 @@ function EmailDetailFilter(message, {
     'class': message
   }
 
+  // Debug
+  if(debug){
+    console.log({
+      message_id: msg.id,
+      sender: msg.from,
+      subject: msg.subject,
+      time: msg.date.toLocaleString('vi')      
+    })
+  }
+
   let isMatched = false
   let globalMatch = {matched: false}
   let sheetMatched = {} // to run customFuntions
@@ -158,7 +44,7 @@ function EmailDetailFilter(message, {
       break
     }
   }
-  if(!isMatched && useGlobalFilter) globalMatch = globalFilter(msg)
+  if(!isMatched && useGlobalFilter) globalMatch = globalFilterMatch(msg, globalFilter)
 
   if(isMatched || globalMatch.matched){
       try{
@@ -187,6 +73,7 @@ function EmailDetailFilter(message, {
 
 
 function EmailFilter({
+  debug = false,
   sheetId = '',
   sheetName = '',
   path = {/*
@@ -204,92 +91,33 @@ function EmailFilter({
   customFuntions = null
 } = {}){
   
+  // custom filter functions
   const customFilter = {
     from,
     subject,
     body
   }
 
-  // Not found filter input, try to get from sheet
+  // Get global filter
+  let globalFilter = []
+  if(useGlobalFilter){
+    globalFilter = getGlobalFilter()
+  }
+
+  // Try to get filters from sheet
   if(!TTools.isValidArray(filter)){
-    filter = TTools.BillSheet({sheetId, sheetName, path}).getAsJSON()
+    filter = getFilterFromSheet(sheetId, sheetName, path)
   }
   
   getAllMessages({}, message => {
     EmailDetailFilter(message, {
+      debug,
       filter,
       customFilter,
       customFuntions,
-      useGlobalFilter
+      useGlobalFilter,
+      globalFilter
     })
   })
   
-}
-
-
-
-
-function getAllMessages({
-  includeSpam = true,
-  includeTrash = false,
-  limit = 0
-} = {},
-callback){
-
-  let msgIds = []
-  let msgs = []
-  const opt = {}
-  
-  if(includeSpam) opt.includeSpamTrash = true
-  if(limit > 0) opt.maxResults = limit
-
-  // Try with Gmail API
-  try {
-    let pageToken
-    do {
-      const threadList = Gmail.Users.Threads.list('me', {
-        pageToken: pageToken,
-        ...opt
-      })
-      if (threadList.threads && threadList.threads.length) {
-        threadList.threads.forEach(thread => {
-          const id = thread.id
-          if(id) msgIds.push(id)
-        })
-      }
-      pageToken = threadList.nextPageToken
-    } while (pageToken)
-  } catch (err) { console.warn(err) }
-  if(msgIds.length){
-    msgIds.forEach(msgId => {
-      try{
-        const msg = GmailApp.getMessageById(msgId)
-        if(TTools.isValidObject(msg)){
-          if(callback && typeof callback === 'function'){
-            if(!includeTrash && msg.isInTrash()) return
-            callback(msg)
-          }
-          msgs.push(msg)
-        }
-      }catch(err) { console.warn(err) }
-    })
-  }
-
-  // Use basic GmailApp
-  else{
-    try{
-      GmailApp.getInboxThreads().forEach(thread => {
-        const messages = thread.getMessages()
-        if(callback && typeof callback === 'function'){
-          messages.forEach(msg => {
-            if(!includeTrash && msg.isInTrash()) return
-            callback(msg)
-          })
-        }
-        msgs = [...msgs, ...messages]
-      })
-    } catch(err) { console.warn(err) }
-  }
-
-  return msgs
 }
